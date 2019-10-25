b403d257a90282e6ac237b9060c39623e1965532
Sylvain Fraïssé <fraisse.sylvain@gmail.com>
2019-10-25 19:24:38 +0200

    :construction: improve then linkMergeRequestToFeature implementation

--- src/Releasy/Development/Domain.fs
+++ src/Releasy/Development/Domain.fs
@@ -4,12 +4,13 @@ open System.Text.RegularExpressions
 open Releasy.Development.Model
 
 let (|Regex|_|) pattern input =
-    let regexMatch = Regex.Match(input, pattern)
+    let regexMatch = Regex.Match(input, pattern, RegexOptions.IgnoreCase)
     if regexMatch.Success then Some(List.tail [ for g in regexMatch.Groups -> g.Value ])
     else None
 
 let linkMergeRequestToFeature (mergeRequest: MergeRequest) : MRLinkedEvent =
     match mergeRequest.description with
-    | Regex @"closes ([\S]*)" [ featureIdentifier ] ->
+    | Regex @"clos(?:es?|ing)\s+(\S*)" [ featureIdentifier ] ->
         MRLinkedToFeature { mergeRequest = mergeRequest; featureIdentifier = featureIdentifier }
     | _ -> MRNotLinkedToAnyFeature { mergeRequest = mergeRequest }
+ 
--- tests/Releasy.Test/Development/Domain.Test.fs
+++ tests/Releasy.Test/Development/Domain.Test.fs
@@ -6,6 +6,21 @@ open System
 open Releasy.Development.Model
 open Releasy.Development.Domain
 
+let  featureIdentifier = "https://trello.com/c/INfJJRKk/1-link-mr-to-close-a-feature-using-closes-or-fixes"
+
+let linkMergeRequestToFeatureShouldLinkMRToFeature (mrDescription: string): Unit =
+  let mergeRequestWhichClosesAFeature =
+    {
+      title = "Test";
+      description = mrDescription;
+      url = new Uri("https://github.com/xxxx/42")
+    }
+  let mrLinkEvent = linkMergeRequestToFeature mergeRequestWhichClosesAFeature
+  match mrLinkEvent with 
+  | MRLinkedToFeature event ->
+      Expect.equal event.featureIdentifier featureIdentifier "Wrong Identifier"
+  | _ -> Expect.isTrue false ("Wrong event type for " + mrDescription)
+
 [<Tests>]
 let tests =
   testList "linkMergeRequestToFeature" [
@@ -34,17 +49,21 @@ let tests =
           Expect.equal event.mergeRequest mergeRequestWhichClosesAFeature "Wrong MR"
       | _ -> Expect.isTrue false "Wrong event type"
 
-    testCase "should define the feature identifier" <| fun _ ->
-      let featureIdentifier = "https://trello.com/c/INfJJRKk/1-link-mr-to-close-a-feature-using-closes-or-fixes"
-      let mergeRequestWhichClosesAFeature =
-        {
-          title = "Test";
-          description = "This PR closes " + featureIdentifier;
-          url = new Uri("https://github.com/xxxx/42")
-        }
-      let mrLinkEvent = linkMergeRequestToFeature mergeRequestWhichClosesAFeature
-      match mrLinkEvent with 
-      | MRLinkedToFeature event ->
-          Expect.equal event.featureIdentifier featureIdentifier "Wrong Identifier"
-      | _ -> Expect.isTrue false "Wrong event type"
-  ]
+    testCase "should link MR to feature with `xxx closes <id>`" <| fun _ -> 
+      linkMergeRequestToFeatureShouldLinkMRToFeature ("This PR closes " + featureIdentifier)
+
+    testCase "should link MR to feature with `closes <id> xxx`" <| fun _ -> 
+      linkMergeRequestToFeatureShouldLinkMRToFeature ("closes " + featureIdentifier + " :tada")
+
+    testCase "should link MR to feature with `close <id> xxx`" <| fun _ -> 
+      linkMergeRequestToFeatureShouldLinkMRToFeature ("close " + featureIdentifier + " :tada")
+
+    testCase "should link MR to feature with `close 	 <id> xxx`" <| fun _ -> 
+      linkMergeRequestToFeatureShouldLinkMRToFeature ("close 	" + featureIdentifier + " :tada")
+
+    testCase "should link MR to feature with `closing <id>`" <| fun _ -> 
+      linkMergeRequestToFeatureShouldLinkMRToFeature ("closing " + featureIdentifier)
+
+    testCase "should link MR to feature with `CLose <id>`" <| fun _ -> 
+      linkMergeRequestToFeatureShouldLinkMRToFeature ("CLose " + featureIdentifier)
+]
