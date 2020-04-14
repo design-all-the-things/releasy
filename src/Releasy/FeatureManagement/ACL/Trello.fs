ea9b218d90c1728d391d02da48c1f1c80ecc70d2
Jérôme Avoustin <jerome@rehia.fr>
2020-04-14 18:34:06 +0200

    :recycle:(trello): refactor linkMRToFeature to use an AsyncResult instead of a Task

--- src/Releasy/FeatureManagement/ACL/Trello.fs
+++ src/Releasy/FeatureManagement/ACL/Trello.fs
@@ -2,13 +2,15 @@ module Releasy.FeatureManagement.ACL.Trello
 
 open System
 open FsConfig
-open FSharp.Control.Tasks.V2.ContextInsensitive
 open Releasy.FeatureManagement.Model
 open Hopac
 open HttpFs.Client
-open System.Threading.Tasks
+open FsToolkit.ErrorHandling
 
-exception TrelloException of string
+type TrelloError =
+  | NetworkError of e:exn
+  | RequestError of message:string
+  | ConfigError of ConfigParseError
 
 [<Convention("RELEASY_TRELLO")>]
 type TrelloConfig = {
@@ -16,25 +18,27 @@ type TrelloConfig = {
   Token : string
 }
 
-let readConfig : Task<TrelloConfig> =
-  match EnvConfig.Get<TrelloConfig>() with
-    | Ok config -> Task.FromResult config
-    | Error e -> TrelloException (e.ToString()) |> Task.FromException<TrelloConfig>
+let readConfig =
+  EnvConfig.Get<TrelloConfig>()
+    |> Result.mapError ConfigError
+    |> Async.result
+
+let bodyOn2xxOr3xx (response: Response) =
+  match response.statusCode with
+  | code when code < 400 -> response |> Response.readBodyAsString |> Job.map Ok
+  | _                    -> response |> Response.readBodyAsString |> Job.map (RequestError >> Error)
 
 let checkOrCreateChecklist (config : TrelloConfig) =
   Request.createUrl Get "https://api.trello.com/1/cards/6QrSHK8z/checklists"
     |> Request.queryStringItem "key" config.ApiKey
     |> Request.queryStringItem "token" config.Token
-    |> Request.responseAsString
+    |> tryGetResponse
+    |> Job.map (Choice.toResult >> (Result.mapError NetworkError))
+    |> JobResult.bind bodyOn2xxOr3xx
     |> Job.toAsync
 
-let makeTheLink (config : TrelloConfig) = task {
-  printfn "coucou !!"
-  let! checklist = checkOrCreateChecklist config |> Async.StartAsTask
-  printfn "API KEY: %s, checklist: %s" config.ApiKey checklist
-}
-
-let linkMergeRequestToFeatureInTrello = task {
+let linkMergeRequestToFeatureInTrello = asyncResult {
   let! config = readConfig
-  (makeTheLink config).Wait()
+  let! checklist = checkOrCreateChecklist config
+  printfn "API KEY: %s, checklist: %s" config.ApiKey checklist
 }
--- tests/Releasy.Test/FeatureManagement/ACL/Trello.Test.fs
+++ tests/Releasy.Test/FeatureManagement/ACL/Trello.Test.fs
@@ -3,10 +3,14 @@ module Releasy.FeatureManagement.ACL.Trello.Test
 open System
 open Expecto
 open Releasy.FeatureManagement.ACL.Trello
+open FsToolkit.ErrorHandling
+open System.Threading.Tasks
 
 [<Tests>]
 let tests =
   testList "linkMergeRequestToFeatureInTrello" [
     testCase "should print related env vars" <| fun _ ->
-      linkMergeRequestToFeatureInTrello.Wait()
+      linkMergeRequestToFeatureInTrello
+        |> Async.StartAsTask
+        |> (fun task -> task.Wait())
   ]
