258b9716d62ca298324e9f6cdf5e65d44a4a63ec
Jérôme Avoustin <jerome@rehia.fr>
2020-04-22 00:10:41 +0200

    :white_check_mark:(trello): add assertion to map error when linking MR to feature

--- tests/Releasy.Test/FeatureManagement/ACL/Trello/Service.Test.fs
+++ tests/Releasy.Test/FeatureManagement/ACL/Trello/Service.Test.fs
@@ -11,6 +11,6 @@ let tests =
   testList "linkMergeRequestToFeatureInTrello" [
     testCase "should print related env vars" <| fun _ ->
       linkMergeRequestToFeatureInTrello
-        |> Async.StartAsTask
-        |> (fun task -> task.Wait())
+        |> Async.RunSynchronously
+        |> (fun result -> Expect.isOk result "The result should not be an error")
   ]
